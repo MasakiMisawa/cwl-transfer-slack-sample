a4aee75e879b5e05b37656a82ecb83963bfaa1ae
MasakiMisawa <>
2021-01-01 02:22:53 +0900

    rename sample function and subscription filter.

--- src/ops/cwl/subscription_filter/locals.tf
+++ src/ops/cwl/subscription_filter/locals.tf
@@ -7,9 +7,9 @@ locals {
   }
 
   cwl_transfer_firehose_subscription_filter = {
-    name            = "cwl-transfer-slack-sample-function-subscription-filter"
+    name            = "cwl-transfer-firehose-subscription-filter"
     role_arn        = module.cwl_transfer_firehose_iam_role.iam_role_arn
-    log_group_name  = "/aws/lambda/cwl-transfer-slack-sample-function"
+    log_group_name  = "/aws/lambda/output-cloudwatch-logs-sample-function"
     filter_pattern  = "?Error ?ERROR"
     destination_arn = data.terraform_remote_state.firehose.outputs.stream_arn
     distribution    = "ByLogStream"
